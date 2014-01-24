09a93a011c736d7ab46f68e1520a2f52264065f6
dongxu <huang@wandoujia.com>
2014-01-23 21:57:20 -0800

    add add handler

--- go-demo/third_server.go
+++ go-demo/third_server.go
@@ -5,6 +5,7 @@ import (
     "math/rand"
     "strconv"
     "runtime"
+    "time"
 )
 
 func init() {
@@ -12,11 +13,23 @@ func init() {
 }
 
 func RandomHandler(w http.ResponseWriter, req *http.Request) {
+    time.Sleep(time.Millisecond * 200)
     w.Write([]byte(strconv.Itoa(rand.Intn(100))))
+}
+
+func AddHandler(w http.ResponseWriter, req *http.Request) {
+    req.ParseForm()
+    p1 := req.Form.Get("p1")
+    p2 := req.Form.Get("p2")
+    num1, _ := strconv.Atoi(p1)
+    num2, _ := strconv.Atoi(p2)
+    time.Sleep(time.Millisecond * 200)
+    w.Write([]byte(strconv.Itoa(num1 + num2)))
 }
 
 func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())
     http.HandleFunc("/random", RandomHandler)
+    http.HandleFunc("/add", AddHandler)
     http.ListenAndServe(":8001", nil)
 }
